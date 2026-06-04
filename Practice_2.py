270601a748cb69979a515b0a46ba062cffd6a6b5
ahsan Naeem <ahsannaeem00119>
2026-06-04 12:51:45 +0100

    task marking done

--- Practice_2.py
+++ Practice_2.py
@@ -5,7 +5,8 @@ while True:
         print("1. Add Task")
         print("2. View Tasks")
         print("3. Remove Task")
-        print("4. Exit")
+        print("4. Mark Tasks as Completed")
+        print("5. Exit")
         
         choice = input("Enter your choice (1-4): ")
         
@@ -33,12 +34,22 @@ while True:
                         number=int(input("Enter the number of the task to remove: "))
                         tasks.pop(number-1)
                         print("Task removed from the list.")
-                        
-            elif choice == '4':
+            
+            elif choice=="4" :
+                if len(tasks) >= 5:
+                    print("Tasks :",tasks)
+                    print("Mark tasks as completed")
+                else:
+                    print("You have less than 5 tasks. Keep going!")
+                              
+            elif choice == '5':
                 print("Exiting the to-do list manager. Goodbye!")
                 break
             else:
-                print("Invalid choice. Please enter a number between 1 and 4.")
+                print("Invalid choice. Please enter a number between 1 and 5.")
+                
+            
+                    
         except ValueError:
             print("Invalid input. Please enter a valid number.")
             
